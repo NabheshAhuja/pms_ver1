75452f10396bf943cff4546aa79a1f2275c9167f
NabheshAhuja <nabhesh.a.insnapsys@gmail.com>
2022-09-14 15:44:28 +0530

    Theme Integration of dashboard

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,39 @@
+<footer class="footer footer-black  footer-white ">
+    <div class="container-fluid">
+        <div class="row">
+            <nav class="footer-nav">
+
+                <ul>
+
+                    <li>
+                        <a href="{{ route('') }}" target="_blank">{{ __('Privacy Policy') }}</a>
+                    </li>
+                    <li>
+                        <a href="https://updivision.com" target="_blank">{{ __('T & C') }}</a>
+                    </li>
+                    <li>
+                        <a href="http://blog.creative-tim.com/" target="_blank">{{ __('contact Us') }}</a>
+                    </li>
+                    <li>
+                        <p> Copyright Ⓒ NabheshAhuja </p>
+                    </li>
+
+                </ul>
+
+            </nav>
+            {{-- <div class="credits ml-auto">
+                <span class="copyright">
+                    ©
+                    <script>
+                        document.write(new Date().getFullYear())
+                    </script>{{ __(', made with ') }}<i
+                        class="fa fa-heart heart"></i>{{ __(' by ') }}<a
+                        class="@if (Auth::guest()) text-white @endif" href="https://www.creative-tim.com"
+                        target="_blank">{{ __('Creative Tim') }}</a>{{ __(' and ') }}<a
+                        class="@if (Auth::guest()) text-white @endif" target="_blank"
+                        href="https://updivision.com">{{ __('UPDIVISION') }}</a>
+                </span>
+            </div> --}}
+        </div>
+    </div>
+</footer>
